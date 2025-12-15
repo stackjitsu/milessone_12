d284dcca90d5f1136c201f5584fe8bb744088bbc
stackjitsu <mwangimark900@gmail.com>
2025-12-15 17:06:04 +0300

    Initial commit

---
+++ script2.js
@@ -0,0 +1,5 @@
+/* script.js */
+(function(){
+  // small utilities: fill year and allow easy replacement of contact fields
+  document.getElementById('year').textContent = new Date().getFullYear();
+})();
